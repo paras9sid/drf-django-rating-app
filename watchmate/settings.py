e59a828d99eb87ea25fdeb5211d9e37ee6c6e4a7
Sidharth Jain <sidharthjainp9@gmail.com>
2025-08-13 14:00:51 +0530

    sk uploaded for solving wsgi error

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY='django-insecure-dg3e*grlz$a@-#s4s^s@d)4#d*!)rzju9%h$0njzh#gu_nh)xv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
